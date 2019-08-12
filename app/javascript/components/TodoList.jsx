c63c22ebec964a50b7bb931241e51fa973a452c3
Simon Luppescu <simon.luppescu@gmail.com>
2019-08-12 14:54:35 +0900

    Turned TodoList component into SFC

--- app/javascript/components/TodoList.jsx
+++ app/javascript/components/TodoList.jsx
@@ -5,46 +5,35 @@ import Container from "./nes/Container";
 import TodoListTitle from "./TodoListTitle";
 import TodoItemContainer from "../containers/TodoItemContainer";
 
-class TodoList extends Component {
-  constructor(props) {
-    super(props);
+const TodoList = props => {
+  const { id: todoListId, title, todoItems } = props.todoList;
 
-    this.handleAddTodoItem = this.handleAddTodoItem.bind(this);
-  }
-
-  handleAddTodoItem() {
-    this.props.onAddTodoItem(this.props.todoList.id);
-  }
-
-  render() {
-    const { id: todoListId, title, todoItems } = this.props.todoList;
-    return (
-      <Container className="todo-list-container is-rounded">
-        <TodoListTitle
-          todoListId={todoListId}
-          value={title}
-          onEditTitle={this.props.onEditTitle}
-        />
-        <div className="todo-list">
-          {Object.keys(todoItems).map(todoItemId => (
-            <TodoItemContainer
-              key={todoItemId}
-              id={todoItemId}
-              todoListId={todoListId}
-              value={todoItems[todoItemId].value}
-              isChecked={todoItems[todoItemId].isChecked}
-              handleCheckTodoItem={this.props.handleCheckTodoItem}
-              handleEditTodoItem={this.props.handleEditTodoItem}
-              onDeleteTodoItem={this.props.onDeleteTodoItem}
-            />
-          ))}
-        </div>
-        <Button variant="primary" onClick={this.handleAddTodoItem}>
-          Add Todo
-        </Button>
-      </Container>
-    );
-  }
-}
+  return (
+    <Container className="todo-list-container is-rounded">
+      <TodoListTitle
+        todoListId={todoListId}
+        value={title}
+        onEditTitle={props.onEditTitle}
+      />
+      <div className="todo-list">
+        {Object.keys(todoItems).map(todoItemId => (
+          <TodoItemContainer
+            key={todoItemId}
+            id={todoItemId}
+            todoListId={todoListId}
+            value={todoItems[todoItemId].value}
+            isChecked={todoItems[todoItemId].isChecked}
+            handleCheckTodoItem={props.handleCheckTodoItem}
+            handleEditTodoItem={props.handleEditTodoItem}
+            onDeleteTodoItem={props.onDeleteTodoItem}
+          />
+        ))}
+      </div>
+      <Button variant="primary" onClick={() => props.onAddTodoItem(todoListId)}>
+        Add Todo
+      </Button>
+    </Container>
+  );
+};
 
 export default TodoList;
